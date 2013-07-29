e0783494d1f691927702b7cec1980341aa6287f3
Kazuya Hatano <k28@me.com>
2013-07-30 08:15:35 +0900

    ex13-7, ex13-8

--- test.pl
+++ test.pl
@@ -1,60 +1,16 @@
 #! /usr/bin/perl
 ## Copyright (C) 2013 by K.Hatano
+#
+#	Search current directory symlink path.
+#
 
 use strict;
 use warnings;
 
-my $latestFile;
-my $oldestDate = 0;
-
-die print "usage : $0 {file list}\n" unless @ARGV;
-#if ( @ARGV == 0 ) {
-#    print "usage : $0 {file list}\n";
-#    exit 1;
-#}
-
-foreach (@ARGV) {
-	my $date = &lastModifiedDate($_);
-	($latestFile, $oldestDate) = ($_, $date)
-		if $date > $oldestDate;
-#    if ($date > $oldestDate) {
-#        $latestFile = $_;
-#        $oldestDate = $date;
-#    }
-}
-
-printf "Oldest date is %s, file is %.1f old.\n", $latestFile , $oldestDate;
-
-# -------------------------- #
-
-sub lastModifiedDate {
-	my $file = shift @_;
-	return (-M $file);
-}
-
-sub checkFilePermission {
-	my $path = shift @_;
-	chomp;
-	print "$path is ";
-	if ( ! -e $path ) {
-		print " not exist\n";
-		return 0;
+foreach (<*>) {
+	if ( -l $_ ) {
+		my $where = readlink;
+		print "$_ -> $where\n";
 	}
-	if ( -r _ ) {
-		print "R";
-	} else {
-		print "-";
-	}
-	if ( -w _ ) {
-		print "W";
-	} else {
-		print "-";
-	}
-	if ( -x _ ) {
-		print "X";
-	} else {
-		print "-";
-	}
-	print "\n";
 }
 
