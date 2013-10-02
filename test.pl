4a5d71d576b93bbc5fa7a7ae9fedabed9269aa0b
Kazuya Hatano <k28@me.com>
2013-10-03 06:50:12 +0900

    ex17-1 to ex17-3

--- test.pl
+++ test.pl
@@ -6,11 +6,26 @@
 
 use strict;
 use warnings;
+use utf8;
+use 5.010;
+use autodie;
+
+sub timeToString {
+	my $time = shift @_;
+	my ($aY, $aM, $aD) = (localtime $time)[5, 4, 3];
+	$aY += 1900;
+	$aM += 1;
+	my $atimeString = sprintf '%4d-%02d-%02d', $aY, $aM, $aD;
+
+	return $atimeString;
+}
 
 foreach (<*>) {
-	if ( -l $_ ) {
-		my $where = readlink;
-		print "$_ -> $where\n";
-	}
+	chomp;
+	my ($atime, $mtime) = (stat $_)[8, 9];
+	my $atimeString = timeToString($atime);
+	my $mtimeString = timeToString($mtime);
+	print "$_	$atimeString	$mtimeString\n";
 }
+
 
